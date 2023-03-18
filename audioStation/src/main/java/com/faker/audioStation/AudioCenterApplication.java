d2f60757ad27ab78c6392d1e17c71ba4fc20a514
an <anlinxi@gmail.com>
2023-03-18 19:59:32 +0800

    jenkins打包

--- audioStation/src/main/java/com/faker/audioStation/AudioCenterApplication.java
+++ audioStation/src/main/java/com/faker/audioStation/AudioCenterApplication.java
@@ -1,8 +1,10 @@
 package com.faker.audioStation;
 
 import com.faker.audioStation.util.TestStatut;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -22,6 +24,8 @@ public class AudioCenterApplication {
         String ip = InetAddress.getLocalHost().getHostAddress();
         String port = env.getProperty("server.port");
         String property = env.getProperty("server.servlet.context-path");
+        //网易云音乐API地址
+        String music163Api = env.getProperty("faker.music163Api");
         String path = property == null ? "" : property;
         TestStatut.ROOT_URL = "http://" + ip + ":" + port + path + "";
         log.info(
@@ -30,6 +34,7 @@ public class AudioCenterApplication {
                         "项目已经启动:\n\t" +
                         "本地本地路径: \thttp://localhost:" + port + path + "/\n\t" +
                         "外网访问路径: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                        "网易云音乐API:\t" + music163Api + "/\n\t" +
                         "------------------------------------------------------------");
 
     }
